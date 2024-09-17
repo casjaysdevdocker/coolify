4ba5445345146b49bf167d6b015209c907a823c1
casjay <git-admin@casjaysdev.pro>
2024-09-17 14:42:18 -0400

    🗃️ Committing everything that changed 🗃️
    rootfs/root/docker/setup/02-packages.sh

--- rootfs/root/docker/setup/02-packages.sh
+++ rootfs/root/docker/setup/02-packages.sh
@@ -33,7 +33,7 @@ install -m 0755 -d /etc/apt/keyrings
 curl -q -LSsf "https://download.docker.com/linux/debian/gpg" -o "/etc/apt/keyrings/docker.asc"
 chmod a+r /etc/apt/keyrings/docker.asc
 printf '%s %s\n' "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian" "$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
-apt-get update
+apt-get update && apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Main script
 
